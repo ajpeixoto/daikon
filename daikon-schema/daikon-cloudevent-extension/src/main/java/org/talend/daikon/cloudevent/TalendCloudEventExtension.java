ee264c9fc9dd05a6a6f18aaafc4671bee4611c9c
YohannTalend <43137818+YohannTalend@users.noreply.github.com>
2023-11-29 15:25:10 +0100

    chore(TPOPS-6824): Enrich talendcloudeventextension with new extensions for Qlik support (#1046)
    **What is the problem this Pull Request is trying to solve?**
    **What is the chosen solution to this problem?**
    
    Enrich talendcloudeventextension with new extensions :
    
    - userid
    - ownerid
    - clientid
    - description
    
    These extensions are mandatory for Qlik
    
    + Upgrade CloudEvent lib version
    
     
    **Link to the JIRA issue**
    <!--e.g. https://jira.talendforge.org/browse/TPOPS-6824 -->
     
    **Please check if the Pull Request fulfills these requirements**
    - [x] The PR commit message follows our
    [guidelines](https://github.com/Talend/daikon/blob/master/CONTRIBUTING.md)
    - [x] Tests for the changes have been added (for bug fixes / features,
    coverage should be over 75% in the new code)
    - [ ] Docs have been added / updated (for bug fixes / features)
    - [ ] Related design / discussions / pages (not in JIRA), if any, are
    all linked or available in the Pull Request
    
    <!-- You can add more checkboxes here -->
     
    **[ ] This Pull Request introduces a breaking change**
     
    <!-- **Original Template** -->
    <!--
    https://github.com/Talend/tools/blob/master/tools-root-github/.github/PULL_REQUEST_TEMPLATE.md
    -->

--- daikon-schema/daikon-cloudevent-extension/src/main/java/org/talend/daikon/cloudevent/TalendCloudEventExtension.java
+++ daikon-schema/daikon-cloudevent-extension/src/main/java/org/talend/daikon/cloudevent/TalendCloudEventExtension.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.StringJoiner;
-import java.util.UUID;
 
 import io.cloudevents.CloudEventExtension;
 import io.cloudevents.CloudEventExtensions;
@@ -30,11 +29,40 @@ public class TalendCloudEventExtension implements CloudEventExtension {
      */
     public static final String CORRELATIONID = "correlationid";
 
-    private static final Set<String> KEY_SET = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(TENANTID, CORRELATIONID)));
+    /**
+     * The key of the {@code ownerid} extension
+     */
+    public static final String OWNERID = "ownerid";
+
+    /**
+     * The key of the {@code userid} extension
+     */
+    public static final String USERID = "userid";
+
+    /**
+     * The key of the {@code clientid} extension
+     */
+    public static final String CLIENTID = "clientid";
+
+    /**
+     * The key of the {@code description} extension
+     */
+    public static final String DESCRIPTION = "description";
+
+    private static final Set<String> KEY_SET = Collections
+            .unmodifiableSet(new HashSet<>(Arrays.asList(TENANTID, CORRELATIONID, OWNERID, USERID, CLIENTID, DESCRIPTION)));
 
     private String tenantid;
 
     private String correlationid;
+
+    private String ownerid;
+
+    private String userid;
+
+    private String clientid;
+
+    private String description;
 
     @Override
     public void readFrom(CloudEventExtensions cloudEventExtensions) {
@@ -46,6 +74,22 @@ public class TalendCloudEventExtension implements CloudEventExtension {
         if (ts != null) {
             this.correlationid = ts.toString();
         }
+        Object to = cloudEventExtensions.getExtension(OWNERID);
+        if (to != null) {
+            this.ownerid = to.toString();
+        }
+        Object tu = cloudEventExtensions.getExtension(USERID);
+        if (tu != null) {
+            this.userid = tu.toString();
+        }
+        Object tc = cloudEventExtensions.getExtension(CLIENTID);
+        if (tc != null) {
+            this.clientid = tc.toString();
+        }
+        Object td = cloudEventExtensions.getExtension(DESCRIPTION);
+        if (td != null) {
+            this.description = td.toString();
+        }
     }
 
     @Override
@@ -55,6 +99,14 @@ public class TalendCloudEventExtension implements CloudEventExtension {
             return this.tenantid;
         case CORRELATIONID:
             return this.correlationid;
+        case OWNERID:
+            return this.ownerid;
+        case USERID:
+            return this.userid;
+        case CLIENTID:
+            return this.clientid;
+        case DESCRIPTION:
+            return this.description;
         }
         throw ExtensionUtils.generateInvalidKeyException(this.getClass(), key);
     }
@@ -80,10 +132,43 @@ public class TalendCloudEventExtension implements CloudEventExtension {
         this.correlationid = correlationid;
     }
 
+    public String getOwnerid() {
+        return ownerid;
+    }
+
+    public void setOwnerid(String ownerid) {
+        this.ownerid = ownerid;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getClientid() {
+        return clientid;
+    }
+
+    public void setClientid(String clientid) {
+        this.clientid = clientid;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public String toString() {
         return new StringJoiner(", ", TalendCloudEventExtension.class.getSimpleName() + "[", "]").add("tenantid=" + tenantid)
-                .add("correlationid='" + correlationid + "'").toString();
+                .add("correlationid='" + correlationid + "'").add("ownerid='" + ownerid + "'").add("userid='" + userid + "'")
+                .add("clientid='" + clientid + "'").add("description='" + description + "'").toString();
     }
 
     @Override
@@ -93,11 +178,13 @@ public class TalendCloudEventExtension implements CloudEventExtension {
         if (o == null || getClass() != o.getClass())
             return false;
         org.talend.daikon.cloudevent.TalendCloudEventExtension that = (org.talend.daikon.cloudevent.TalendCloudEventExtension) o;
-        return Objects.equals(getCorrelationid(), that.getCorrelationid()) && Objects.equals(getTenantid(), that.getTenantid());
+        return Objects.equals(getCorrelationid(), that.getCorrelationid()) && Objects.equals(getOwnerid(), that.getOwnerid())
+                && Objects.equals(getClientid(), that.getClientid()) && Objects.equals(getUserid(), that.getUserid())
+                && Objects.equals(getDescription(), that.getDescription());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getTenantid(), getCorrelationid());
+        return Objects.hash(getTenantid(), getCorrelationid(), getOwnerid(), getUserid(), getClientid(), getDescription());
     }
 }
